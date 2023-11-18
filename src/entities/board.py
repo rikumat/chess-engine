a87d1c771ffbd691f6aca05b10294eee10a6fa57
Riku Mattila <riku.p.mattila@gmail.com>
2023-11-18 23:05:58 +0200

    added docstring for board methods

--- src/entities/board.py
+++ src/entities/board.py
@@ -6,12 +6,20 @@ class Board():
         pass
 
     def get_vertical_and_horizontal_moves(self, board, square):
+        """
+        calculate vertical and horizontal moves.
+        Used to calculate moves for rooks and queen.
+        """
         moves = []
         coordinates = utils.square_to_coordinates(square)
         for i in range(coordinates[0], 8):
             pass
 
     def get_diagonal_moves(self, board, square):
+        """
+        calculates legal diagonal moves from square on a chessboard.
+        Used for calculating queen and bishop moves.
+        """
         coordinates = utils.square_to_coordinates(square)
         right_bottom_limit = 8-max(coordinates)
 
