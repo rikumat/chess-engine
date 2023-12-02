eeebd4bbe6357bd45caf08d02fb8dbb7fbb10e6e
Riku Mattila <riku.p.mattila@gmail.com>
2023-12-02 21:26:57 +0200

    changed board visualization, added command panel

--- src/index.py
+++ src/index.py
@@ -1,5 +1,5 @@
 import chess
-
+import utils
 from services.engine import Engine
 from services.ai import Ai
 
@@ -8,8 +8,24 @@ chess_engine = Engine()
 ai = Ai()
 
 print(chess_engine.board)
+
+editor_open = False
+
 while True:
-    move = input("Enter your move ")
+    while editor_open:
+        command = input("Enter command: ")
+        if command == "exit":
+            editor_open = False
+            continue
+        if len(command)==3:
+            coords = utils.square_to_coordinates(command[1:])
+            chess_engine.board[coords[0]][coords[1]]=command[0]
+
+    move = input("Enter your move: ")
+    if move == "editor":
+        editor_open=True
+        continue
+
     if move == "quit":
         break
     result = chess_engine.make_move(move)
@@ -18,7 +34,8 @@ while True:
         ai_move = ai.calculate_move(chess_engine.board, False)
         print(ai_move)
         chess_engine.make_move(ai_move)
-        print(chess_engine.board)
+        for i in chess_engine.board:
+            print(''.join(i))
 
     else:
         print(result)
